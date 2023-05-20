6182bf7cce66b406c237fa735597c490629e3701
WalleyemanZ <jacobzea96@gmail.com>
2023-05-20 14:23:14 -0600

    added code to resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,8 +3,62 @@ const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolver ={
-    Query: {},
-    Mutation: {}
+    Query: {
+        users: async () => {
+            return User.find().populate('books');
+        },
+        user: async (parent, {username}) => {
+            return User.findOne({username}).populate('books');
+        } ,  
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({_id: context.user._id})
+                .select('-__v -password')
+                .populate('books')
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in');
+        }
+
+    },
+    Mutation: {
+        login: async (parent, {email, password}) => {
+            const user = await User.findOne({email});
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const token = signToken(user);
+            return {token, user};
+        },
+        // addUser: async (parent, { username, email,password }) ={
+        //     const user = await User.create({username, email, password});
+        //     const token = signToken(user);
+        //     return {token, user};
+        // },
+        saveBook: async (parent, {bookData}, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    {_id: context.user._id},
+                    {$push: {savedBooks: bookData}},
+                    {new: true}
+                );
+                return updatedUser;
+            }
+        },
+        deleteBook: async (parent, {bookId}, context) => {
+            if (context.user) {
+                return User.findByIdAndUpdate(
+                    {_id: context.user._id},
+                    {$pull: {savedBooks: {bookId}}},
+                    {new: true}
+                );
+            }
+        }
+    }
 };
 
-module.exports = resolvers;
+module.exports = resolver;
