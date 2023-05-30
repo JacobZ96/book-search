90deac399aca2ad4dc5b0073531281f2d59010ac
WalleyemanZ <jacobzea96@gmail.com>
2023-05-29 19:11:46 -0600

    updated files in client folder

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -8,6 +8,7 @@ import {
   Row
 } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
@@ -72,11 +73,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      const { data } = await saveBook({
+        variables: { bookId }
+      });
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
